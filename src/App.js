46f3654e03257a350be59b079cb23bae9db24c7e
joel <joel@optimiz.claims>
2020-02-12 19:27:42 +0100

    Added About and contact us pages

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Layout from './hoc/Layout';
-// import Homepage from './Pages/Homepage';
-// import AboutUsPage from './Pages/AboutUsPage';
-// import OurValuesPage from './Pages/OurValuesPage';
-// import OurCommunitiesPage from './Pages/OurCommunitiesPage';
-// import ContactUsPage from './Pages/ContactUsPage';
+import Homepage from './Pages/Homepage';
+import AboutUsPage from './Pages/AboutUsPage';
+import ContactUsPage from './Pages/ContactUsPage';
 
 function App() {
   return (
     <Layout>
       <Switch>
-        {/* <Route path="/about-us" component={AboutUsPage} />
-        <Route path="/our-values" component={OurValuesPage} />
-        <Route path="/our-communities" component={OurCommunitiesPage} />
-        <Route path="/contact-us" component={ContactUsPage} /> */}
+        <Route path="/about-us" component={AboutUsPage} />
+        <Route path="/contact-us" component={ContactUsPage} />
         <Route path="/" component={Homepage} />
       </Switch>
     </Layout>
---
+++ src/Pages/Homepage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// import Hero from '../Organisms/Hero';
+// import WelcomeMessage from '../Organisms/WelcomeMessage';
+
+
+import Aux from '../hoc/Auxilary';
+
+const Homepage = () => (
+    <Aux>
+        {/* <Hero />
+        <WelcomeMessage /> */}
+        <p>This is the homepage</p>
+    </Aux>   
+);
+
+
+export default Homepage;
