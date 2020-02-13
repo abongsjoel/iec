3aa13cf2fe67a4805103e82e63588c13c4fd6058
joel <joel@optimiz.claims>
2020-02-13 14:47:39 +0100

    Made Hero responsive

--- src/Atoms/HeroButton.js
+++ src/Atoms/HeroButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HeroButton = ({text, link}) => {
 
-    const style = "bg-main-100 font-semibold inline-block py-3 px-16 rounded text-xl text-white";
+    const style = "bg-main-100 font-semibold inline-block py-2 px-8 text-sm sm:px-12 sm:text-lg lg:py-3 lg:px-16 lg:text-xl rounded text-white";
 
     return (
         <a href={link} type="button" className={style}>{text}</a>
--- src/Atoms/HeroMessage.js
+++ src/Atoms/HeroMessage.js
@@ -5,9 +5,33 @@ const HeroMessage = styled.p`
     font-family: "Open Sans";
     color: rgb(255, 255, 255);
     text-transform: uppercase;
-    font-size: 72px;
-    line-height: 80px;
+    font-size: 28px;
+    line-height: 36px;
     text-align: left;
+    
+    @media only screen and (min-width: 400px) {
+        & {
+            font-size: 32px;
+            line-height: 42px;
+        }
+    
+     @media only screen and (min-width: 600px) {
+        & {
+            font-size: 36px;
+            line-height: 48px;
+        }
+        
+      @media only screen and (min-width: 800px) {
+        & {
+            font-size: 48px;
+            line-height: 56px;
+        }
+        
+     @media only screen and (min-width: 1000px) {
+        & {
+            font-size: 72px;
+            line-height: 80px;
+        }
     `;
 
 export default HeroMessage;
