50be918b3815df8bfbfff16d9837085ae229eb5d
joel <joel@optimiz.claims>
2020-02-14 14:32:12 +0100

    Added Quote using icon instead and added title

--- src/Molecules/Testimonial.js
+++ src/Molecules/Testimonial.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-import Quote from "../Atoms/Quote";
 
-const Testimonial = ({title, text, name, address}) => (
+const Testimonial = ({text, name, address}) => (
     <div className="flex justify-center">
         <div>
-            <Quote className="text-main-100">"</Quote>
             <p>This is the Testimonials Area</p>
         </div>
     </div>
--- src/Organisms/Testimonials.js
+++ src/Organisms/Testimonials.js
@@ -1,15 +1,24 @@
 import React from 'react';
 
 import Testimonial from "../Molecules/Testimonial";
+import downloadIcon from "../assets/icons/download.svg";
+import Img from "../Atoms/Img2";
+
+import quoteIcon from "../assets/icons/quote.svg"
+import SectionHeading from "../Atoms/SectionHeading";
 
 const Testimonials = () => (
     <div className="px-1/10 pt-20">
-        <Testimonial
-            title="Testimonials"
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc elit lacus, bibendum in accumsan sed,
-            dignissim at massa. Cras euismod metus sit amet metus mollis sagittis."
-            name="Thierry Joel"
-        />
+        <div className="flex justify-center">
+            <Img className="w-10 text-main-100 " src={quoteIcon} alt="" />
+        </div>
+            <h3 className="font-bold text-3xl text-center">Testimonials</h3>
+            <Testimonial
+                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc elit lacus, bibendum in accumsan sed,
+                dignissim at massa. Cras euismod metus sit amet metus mollis sagittis."
+                name="Thierry Joel"
+            />
+
     </div>
 );
 
